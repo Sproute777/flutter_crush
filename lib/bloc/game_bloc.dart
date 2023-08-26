be92278b00af0d7d8fa402a0d1b4937c0f44feb6
sprt777 <ravarmir@gmail.com>
2023-08-27 00:30:18 +0300

    optimization

--- lib/bloc/game_bloc.dart
+++ lib/bloc/game_bloc.dart
@@ -1,11 +1,7 @@
 import 'dart:async';
 import 'dart:convert';
-import 'package:flutter_crush/controllers/game_controller.dart';
 import 'package:flutter_crush/model/level.dart';
-import 'package:flutter_crush/model/objective.dart';
-import 'package:flutter_crush/model/objective_event.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_crush/model/tile.dart';
 import 'package:logging/logging.dart' hide Level;
 // import 'package:quiver/iterables.dart';
 // import 'package:quiver/quiver.dart';
@@ -33,39 +29,38 @@ class GameBloc {
   //
   // Controller aimed at processing the Objective events
   //
-  PublishSubject<ObjectiveEvent> _objectiveEventsController =
-      PublishSubject<ObjectiveEvent>();
-  Function get sendObjectiveEvent => _objectiveEventsController.sink.add;
-  Stream<ObjectiveEvent> get outObjectiveEvents =>
-      _objectiveEventsController.stream;
+  // PublishSubject<ObjectiveEvent> _objectiveEventsController =
+  //     PublishSubject<ObjectiveEvent>();
+  // void  setObjectiveEvent(ObjectiveEvent event) => _objectiveEventsController.sink.add(event);
+  // Stream<ObjectiveEvent> get outObjectiveEvents =>
+  //     _objectiveEventsController.stream;
 
   //
   // Controller that emits a boolean value to notify that a game is over
   // the boolean value indicates whether the game is won (=true) or lost (=false)
   //
-  PublishSubject<bool> _gameIsOverController = PublishSubject<bool>();
-  Stream<bool> get gameIsOver => _gameIsOverController.stream;
+  // PublishSubject<bool> _gameIsOverController = PublishSubject<bool>();
+  // Stream<bool> get gameIsOver => _gameIsOverController.stream;
 
   //
   // Controller that emits the number of moves left for the game
   //
-  PublishSubject<int> _movesLeftController = PublishSubject<int>();
-  Stream<int> get movesLeftCount => _movesLeftController.stream;
+  // PublishSubject<int> _movesLeftController = PublishSubject<int>();
+  // Stream<int> get movesLeftCount => _movesLeftController.stream;
 
   //
   // List of all level definitions
   //
-  final _levels = <Level>[];
-  int _maxLevel = 0;
-  int _levelNumber = 0;
-  int get levelNumber => _levelNumber;
-  int get numberOfLevels => _maxLevel;
+  final levels = <Level>[];
+
+  int _selectedLevel = 0;
+  int get selectedLevel => _selectedLevel;
 
   //
   // The Controller for the Game being played
   //
-  late GameController _gameController;
-  GameController get gameController => _gameController;
+  // late GameController _gameController;
+  // GameController get gameController => _gameController;
 
   //
   // Constructor
@@ -83,27 +78,26 @@ class GameBloc {
   //
   //  e.g.  bloc.setLevel(1).then(() => )
   //
-  Future<Level?> setLevel(int levelIndex) async {
-    _log.fine('setLevel');
-    _levelNumber = (levelIndex - 1).clamp(0, _maxLevel);
+  // Future<Level?> setLevel(int levelIndex) async {
+  //   _log.fine('setLevel');
+  //   _selectedLevel = (levelIndex - 1).clamp(0, levels.length);
 
-    _log.fine('_levelNumber $_levelNumber');
-    _log.fine('get _gameController');
-    //
-    // Initialize the Game
-    //
-    _gameController = GameController(level: _levels[_levelNumber]);
-    _log.fine('_gameController ${_gameController.level}');
+  //   _log.fine('get _gameController');
+  //   //
+  //   // Initialize the Game
+  //   //
+  //   _gameController = GameController(level: levels[_selectedLevel]);
+  //   _log.fine('_gameController ${_gameController.level}');
 
-    //
-    // Fill the Game with Tile and make sure there are possible Swaps
-    //
-    _log.fine('shufl _gameController');
-   await _gameController.shuffle();
-  //  .timeout(Duration(seconds: 10));
+  //   //
+  //   // Fill the Game with Tile and make sure there are possible Swaps
+  //   //
+  //   _log.fine('shufl _gameController');
+  //  await _gameController.shuffle();
+  // //  .timeout(Duration(seconds: 10));
 
-      return _levels[_levelNumber];
-  }
+  //     return levels[_selectedLevel];
+  // }
 
   //
   // Load the levels definitions from assets
@@ -117,8 +111,7 @@ class GameBloc {
         print(levelItem.toString());
         try {
           final l = Level.fromJson(levelItem);
-          _levels.add(l);
-          _maxLevel += 1;
+          levels.add(l);
         } catch (e,stack) {
          _log.severe('crash during parse fromJson',e,stack);
         }
@@ -133,61 +126,61 @@ class GameBloc {
   // We need to notify anyone who might be interested in
   // knowing it so that actions can be taken
   //
-  void pushTileEvent(TileType? tileType, int counter) {
-    // We first need to decrement the objective by the counter
-    print('ssetLevel');
-    Objective? objective;
-    try {
-      objective =
-          gameController.level.objectives.firstWhere((o) => o.type == tileType);
-    } catch (_) {}
-    if (objective == null) {
-      return;
-    }
+  // void pushTileEvent(TileType? tileType, int counter) {
+  //   // We first need to decrement the objective by the counter
+  //   print('ssetLevel');
+  //   Objective? objective;
+  //   try {
+  //     objective =
+  //         gameController.level.objectives.firstWhere((o) => o.type == tileType);
+  //   } catch (_) {}
+  //   if (objective == null) {
+  //     return;
+  //   }
 
-    objective.decrement(counter);
+  //   objective.decrement(counter);
 
-    // Send a notification
-    sendObjectiveEvent(
-        ObjectiveEvent(type: tileType, remaining: objective.count));
+  //   // Send a notification
+  //   setObjectiveEvent(
+  //       ObjectiveEvent(type: tileType, remaining: objective.count));
 
-    // Check if the game is won
-    bool isWon = true;
-    gameController.level.objectives.forEach((Objective? objective) {
-      if ((objective?.count ?? 0) > 0) {
-        isWon = false;
-      }
-    });
+  //   // Check if the game is won
+  //   bool isWon = true;
+  //   gameController.level.objectives.forEach((Objective? objective) {
+  //     if ((objective?.count ?? 0) > 0) {
+  //       isWon = false;
+  //     }
+  //   });
 
-    // If the game is won, send a notification
-    if (isWon) {
-      _gameIsOverController.sink.add(true);
-    }
-  }
+  //   // If the game is won, send a notification
+  //   if (isWon) {
+  //     _gameIsOverController.sink.add(true);
+  //   }
+  // }
 
   //
   // A move has been played, let's decrement the number of moves
   // left and check if the game is over
   //
-  void playMove() {
-    print('playMode');
-    int movesLeft = gameController.level.decrementMove();
+  // void playMove() {
+  //   print('playMode');
+  //   int movesLeft = gameController.level.decrementMove();
 
-    // Emit the number of moves left (to refresh the moves left panel)
-    _movesLeftController.sink.add(movesLeft);
+  //   // Emit the number of moves left (to refresh the moves left panel)
+  //   _movesLeftController.sink.add(movesLeft);
 
-    // There is no move left, so inform that the game is over
-    if (movesLeft == 0) {
-      _gameIsOverController.sink.add(false);
-    }
-  }
+  //   // There is no move left, so inform that the game is over
+  //   if (movesLeft == 0) {
+  //     _gameIsOverController.sink.add(false);
+  //   }
+  // }
 
   //
   // When a game starts, we need to reset everything
   //
-  void reset() {
-    gameController.level.resetObjectives();
-  }
+  // void reset() {
+  //   gameController.level.resetObjectives();
+  // }
 
   // @override
   // void dispose() {
